fcadadbfdadcbaf0ea1bce9d728e3a5b8c9083b0
SyedHaroonAli <ali.md@teksacademy.com>
2026-05-13 02:05:33 +0530

    Added Solution for Problem 2 in Basic Set 3

--- JS/Solutions/basics/basics_3.js
+++ JS/Solutions/basics/basics_3.js
@@ -12,7 +12,18 @@ const validParenthesis = (string) => {
 }
 
 // Problem 2
-const groupAnagrams = () => {}
+const groupAnagrams = (words) => {
+  const map = new Map()
+  for (const word of words) {
+    const key = word.split('').sort().join('')
+    if (!map.has(key)) {
+      map.set(key, [])
+    }
+    map.get(key).push(word)
+  }
+
+  return Array.from(map.values())
+}
 
 // Problem 3
 const deepCloneObject = (nestedObject) => {
